d873db8584520a450a4ec691a92835394fd2d35d
hamimDev <mdhamimhowladerasif@gmail.com>
2024-03-11 01:15:44 +0600

    fetch-url-option-method-headers-body for post

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,43 @@ function App() {
       .then((res) => res.json())
       .then((data) => setUsers(data));
   }, []);
+
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const from = event.target;
+    const name = from.name.value;
+    const email = from.email.value;
+    const user = { name, email };
+    console.log(user);
+    fetch("http://localhost:5000/users", {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
   return (
     <>
       <h1>User Management System</h1>
       <h4>The number of Users: {users.length}</h4>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" id="" />
+        <br />
+      </form>
+      <div>
+        {users.map((user) => (
+          <p key={user.id}>
+            {user.id}. {user.name} : {user.email}
+          </p>
+        ))}
+      </div>
     </>
   );
 }
